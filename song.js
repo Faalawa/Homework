d4aeecae6bf9b3dbade02bbf5a3824ab3f276886
Awa <awafall1256@gmail.com>
2022-05-04 09:52:44 -0400

    music homework

---
+++ song.js
@@ -0,0 +1,18 @@
+module.exports = [{ 
+  albumNumber: "0", 
+    name:"Sama everything",
+    publishDate:"2020",
+    imgURL:"https://lyricstranslate.com/files/styles/artist/public/viviane_1.png",
+    songTitles:["recce na","Sweet game", "Sama everything", "Reguine tass"]
+ },
+ {  
+   albumNumber: "1",
+    name:"Fans yi",
+    description:"2012",
+    imgURL:"https://i.ytimg.com/vi/FLhWJLa6U5A/maxresdefault.jpg",
+    songTitles:["wuyuma","No stress", "Tere doundou"]
+ }
+];
+
+
+
